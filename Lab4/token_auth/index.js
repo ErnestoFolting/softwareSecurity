73c713a3a77d1bdf23054793c2aecd6788c0f5ba
ErnestoFolting <danilsmy.edu@gmail.com>
2023-10-09 18:05:33 +0300

    Add Auth0 access token getting

--- Lab4/token_auth/index.js
+++ Lab4/token_auth/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const path = require("path");
+const axios = require("axios");
 
 const port = 3000;
 
@@ -8,17 +9,18 @@ const app = express();
 app.use(express.json());
 
 const TOKEN_SECRET = "token-secret";
+const auth0TokenUrl = "https://dev-vs5mll4nqah4fkw0.us.auth0.com/oauth/token";
 
 app.get("/", (req, res) => {
   const token = req.headers["authorization"];
-  if(token){
+  if (token) {
     jwt.verify(token, TOKEN_SECRET, (err, decoded) => {
       if (err) {
         return res.status(401).sendFile(indexPath);
       }
-      return res.status(200).json({login:decoded.login})
+      return res.status(200).json({ login: decoded.login });
     });
-  }else{
+  } else {
     res.sendFile(path.join(indexPath));
   }
 });
@@ -32,25 +34,27 @@ app.get("/logout", (req, res) => {
 app.post("/api/login", (req, res) => {
   const { login, password } = req.body;
 
-  const user = users.find(
-    (user) => user.login == login && user.password == password
-  );
+  const requestBody = {
+    audience: "https://dev-vs5mll4nqah4fkw0.us.auth0.com/api/v2/",
+    grant_type: "password",
+    client_id: "BSvD7ZWDj24bhu5SfGyPIjTAczIk5qsI",
+    client_secret:
+      "YZW9whGXIW-UYd8oSw0q8Q6BWmafY_svTxDE8bAX3bWcHayoXvbuK562HcgzRXzQ",
+    username: login,
+    password: password,
+  };
 
-  if (user) {
-    const token = jwt.sign({ login }, TOKEN_SECRET, { expiresIn: "1h" });
-    res.json({ token });
-  } else {
-    res.status(401).json({ message: "Authentication failed" });
-  }
+  axios
+    .post(auth0TokenUrl, requestBody)
+    .then((response) => {
+      const token = response.data.access_token;
+      res.json({ token });
+    })
+    .catch((error) => {
+      console.log("error " + error);
+      res.status(401).json("Not Authorized");
+    });
 });
-
-const users = [
-  {
-    login: "Login",
-    password: "Password",
-    username: "Username",
-  },
-];
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
